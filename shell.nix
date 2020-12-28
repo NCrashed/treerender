18d398018531fbdd91121ea5201e69d37f3939ed
Anton Gushcha <ncrashed@protonmail.com>
2020-12-29 01:21:37 +0300

    Initial commit

---
+++ shell.nix
@@ -0,0 +1,24 @@
+with import ./nix/pkgs.nix {
+    config = {
+      packageOverrides = pkgs: {
+        dmd = pkgs.callPackage ./nix/dmd {};
+        ldc = pkgs.callPackage ./nix/ldc {};
+      };
+    };
+  };
+
+stdenv.mkDerivation rec {
+  name = "treerender-d-env";
+  env = buildEnv { name = name; paths = buildInputs; };
+
+  buildInputs = [
+    dmd
+    ldc
+    dub
+    SDL2
+    SDL2_mixer
+    valgrind
+    kdeApplications.kcachegrind
+    pkg-config
+  ];
+}
