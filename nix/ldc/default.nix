2caf1238a8b0161def0c63a8c2c831304baea18e
Anton Gushcha <ncrashed@protonmail.com>
2021-01-10 14:33:38 +0300

    Update ldc2

--- nix/ldc/default.nix
+++ nix/ldc/default.nix
@@ -1,4 +1,4 @@
 import ./generic.nix {
-  version = "1.24.0";
-  ldcSha256 = "0g5svf55i0kq55q49awmwqj9qi1n907cyrn1vjdjgs8nx6nn35gx";
+  version = "1.25.0-beta1";
+  ldcSha256 = "0wg454aahp3l43hln8wx6clibf1gyb7qmw7jiwv11jyr0ddkm6ws";
 }
